3b311f7792ed16efe2a5293585417fc5e11a342d
JohanFloyd71 <39989499+Diegofloyd@users.noreply.github.com>
2022-07-26 13:01:51 -0500

    Update de estructura

--- app.js
+++ app.js
@@ -5,24 +5,18 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const { json } = require('body-parser');
 
 var pruebaRouter = require('./routes/prueba');
 
 // Ventanas
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
-<<<<<<< HEAD
 var mainRouter = require('./routes/principal');
 var ctrlUsuariosRouter = require('./routes/dashboardAdmin');
-=======
 var mainRouter = require('./routes/main');
-var dashRouter = require('./routes/dashboard');
-var ctrlUsuariosRouter = require('./routes/ctrlUsuarios');
-var dashRouter = require('./routes/dashboard');
->>>>>>> 8abb40be1b70be16c8d0afcd98ef839f7c2ec6b1
 var logout = require('./routes/logout');
-const fs = require('fs');
-const { json } = require('body-parser');
 var dashClientesRouter = require('./routes/dashboardClientes');
 var users = require('./routes/users');
 
@@ -34,7 +28,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-<<<<<<< HEAD
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -42,7 +35,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: '123456', resave: true, saveUninitialized: true }));
 app.use(express.urlencoded({ extended: false }));
-=======
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
@@ -51,7 +43,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({secret: '123456', resave: true, saveUninitialized: true}));
 app.use(express.urlencoded({extended:false}));
->>>>>>> 8abb40be1b70be16c8d0afcd98ef839f7c2ec6b1
 app.use(express(json));
 
 // ----------------Paginación------------------------//
@@ -59,23 +50,13 @@ app.use('/', indexRouter);
 //variable de prueba
 app.use('/prueba', pruebaRouter);
 //end variable de prueba
-<<<<<<< HEAD
 app.use('/users', users);
 app.use('/principal', mainRouter);
-app.use('/dashboardAdmin', ctrlUsuariosRouter);
 app.use('/login', loginRouter);
-=======
-
-app.use('/users', users);
 app.use('/login', loginRouter);
-app.use('/main', mainRouter);
-app.use('/dashboard', dashRouter);
-app.use('/ctrlUsuarios', ctrlUsuariosRouter);
-app.use('/login', loginRouter);
-app.use('/main', mainRouter);
->>>>>>> 8abb40be1b70be16c8d0afcd98ef839f7c2ec6b1
 app.use('/logout', logout);
 app.use('/dashboardClientes', dashClientesRouter);
+app.use('/dashboardAdmin', ctrlUsuariosRouter);
 
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -4,88 +4,83 @@ var router = express.Router();
 
 
 
-router.get('/servicioObtenerUsuario/:nom_cuenta', (request, response) =>{
+router.get('/servicioObtenerUsuario/:nom_cuenta', (request, response) => {
 
-    nom_cuenta=request.params.nom_cuenta;
+  nom_cuenta = request.params.nom_cuenta;
 
-    pool.query("SELECT * FROM usuarios WHERE nom_cuenta=?", nom_cuenta, (error, result)  =>{
-
-
-        if(error) throw error;
-//response.status(200).send((result[0].id).toString());
-        response.send({nombre:result[0].nombre.toString(),user:result[0].nom_cuenta.toString(), pass:result[0].password.toString(), email:result[0].email.toString() });
+  pool.query("SELECT * FROM usuarios WHERE nom_cuenta=?", nom_cuenta, (error, result) => {
 
 
-    });
+    if (error) throw error;
+    //response.status(200).send((result[0].id).toString());
+    response.send({ nombre: result[0].nombre.toString(), user: result[0].nom_cuenta.toString(), pass: result[0].password.toString(), email: result[0].email.toString() });
+
+
+  });
 
 });
 
 router.post('/servicioEditar', (request, response) => {
-    const nombre = request.body.nombre;
-    const nom_cuenta = request.body.nom_cuenta;
-    const password = request.body.password;
-    const email = request.body.email;
-    
-  
-    pool.query("UPDATE usuarios SET nombre=?, nom_cuenta=?, password=?, email=? WHERE nom_cuenta=?", [nombre,nom_cuenta ,password, email, nom_cuenta], (error, result) => {
-  
-      if(error) throw error;
-  
-      response.send('Se actualizo tu registro');
-  
-      
-    });
+  const nombre = request.body.nombre;
+  const nom_cuenta = request.body.nom_cuenta;
+  const password = request.body.password;
+  const email = request.body.email;
+
+
+  pool.query("UPDATE usuarios SET nombre=?, nom_cuenta=?, password=?, email=? WHERE nom_cuenta=?", [nombre, nom_cuenta, password, email, nom_cuenta], (error, result) => {
+
+    if (error) throw error;
+
+    response.send('Se actualizo tu registro');
+
+
   });
+});
 
 router.post('/servicioRegistrar', (request, response) => {
-    const nombre = request.body.nombre;
-    const nom_cuenta = request.body.nom_cuenta;
-    const password = request.body.password;
-    const email = request.body.email;
-    const imagen = request.body.imagen;
-  
-    pool.query("SELECT * FROM usuarios WHERE nom_cuenta=? or email=?", [nom_cuenta, email], (error, result) => {
-      if (error) throw error;
-  
-      //response.status(200).send((result[0].id).toString());
-      if (result[0] != null) {
-  
-        /*response.redirect("http://localhost:3000/");*/
-        response.send("Este nombre de usuario o email ya estan en uso");
-      } else {
-        pool.query("INSERT INTO usuarios (nombre, nom_cuenta, password, email) VALUES (?,?,?,?)", [nombre, nom_cuenta, password, email], (error, result) => {
-          if (error) throw error;
-  
-          response.send("Agregado correctamente");
-  
-        });
-      };
-    });
+  const nombre = request.body.nombre;
+  const nom_cuenta = request.body.nom_cuenta;
+  const password = request.body.password;
+  const email = request.body.email;
+  const imagen = request.body.imagen;
+
+  pool.query("SELECT * FROM usuarios WHERE nom_cuenta=? or email=?", [nom_cuenta, email], (error, result) => {
+    if (error) throw error;
+
+    //response.status(200).send((result[0].id).toString());
+    if (result[0] != null) {
+
+      /*response.redirect("http://localhost:3000/");*/
+      response.send("Este nombre de usuario o email ya estan en uso");
+    } else {
+      pool.query("INSERT INTO usuarios (nombre, nom_cuenta, password, email) VALUES (?,?,?,?)", [nombre, nom_cuenta, password, email], (error, result) => {
+        if (error) throw error;
+
+        response.send("Agregado correctamente");
+
+      });
+    };
   });
+});
 
 router.post('/servicioLogin', (request, response) => {
-  const nom_cuenta= request.body.nom_cuenta;
-  const password= request.body.password;
-  
-      pool.query("SELECT * FROM usuarios WHERE nom_cuenta=? and password=?",[nom_cuenta,password], (error, result) => {
-      if (error) throw error;
+  const nom_cuenta = request.body.nom_cuenta;
+  const password = request.body.password;
 
-      //response.status(200).send((result[0].id).toString());
-      if(result[0]!=null){
-       
-        /*response.redirect("http://localhost:3000/");*/
-        request.session.nombre=request.body.nom_cuenta;
-    
-<<<<<<< HEAD
-        response.render('principal', { nombre: request.session.nombre });
-=======
-        response.render('main', { nombre: request.session.nombre });
->>>>>>> 8abb40be1b70be16c8d0afcd98ef839f7c2ec6b1
-     
-      }else{
-        response.send("Credenciales erroneas");
-       
-      }
+  pool.query("SELECT * FROM usuarios WHERE nom_cuenta=? and password=?", [nom_cuenta, password], (error, result) => {
+    if (error) throw error;
+
+    //response.status(200).send((result[0].id).toString());
+    if (result[0] != null) {
+
+      /*response.redirect("http://localhost:3000/");*/
+      request.session.nombre = request.body.nom_cuenta;
+      response.render('principal', { nombre: request.session.nombre });
+
+    } else {
+      response.send("Credenciales erroneas");
+
+    }
   });
 });
 
